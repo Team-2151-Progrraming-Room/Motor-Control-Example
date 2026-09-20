fafd4e5f220b475c26d606a24f374b58edfacce7
1Sketch <131012292+ultrabros8190@users.noreply.github.com>
2026-09-20 18:09:53 -0500

    Remove example subsystem and command mappings
    Removed example subsystem and its associated commands from the controller mappings.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final MotorTestingSubsystem m_exampleSubsystem = new MotorTestingSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -48,8 +47,7 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.runMotorCommand());
-    m_driverController.a().whileTrue(m_exampleSubsystem.stopMotor());
+    
   }
 
   /**
